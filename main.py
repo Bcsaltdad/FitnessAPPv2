c3b4821ca03df466e6dcf75ef4e65f3fdd91b66c
Bcsaltdad <160277927+Bcsaltdad@users.noreply.github.com>
2025-04-06 15:37:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import json
 import hashlib
+pip install matplotlib
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from exercise_utils import ExerciseDatabase
